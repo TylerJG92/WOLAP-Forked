11fb4d18afa637d2a3860bf01611ce0b8efc577f
TylerJG92 <tylerteam2660@gmail.com>
2026-08-28 02:40:49 -0700

    Baseline Pathing
    Started the basic routine and pathing to nessicary folders and files for the code that will package the .zip for Thunderstore

---
+++ ModManager_Packager/build-package.ps1
@@ -0,0 +1,28 @@
+# Folder conatining this packaging script
+$packagerRoot = $PSScriptRoot
+# Root folder of the WOLAP Repository
+$repoRoot = Split-Path -Parent $packagerRoot
+# Root to Patcher
+$patchRoot = Join-Path $repoRoot -ChildPath patcher\WOLAP.DependencyPatcher
+# Root for the ref Folder
+$refRoot = Join-Path $packagerRoot "ref"
+# Root for output folder
+$outputRoot = Join-Path $packagerRoot "output"
+# path to main-mod WOLAP.csproj
+$mMWOLAPPath = Join-Path $repoRoot "WOLAP.csproj"
+# Path to main-mod WOLAP.dll
+$mMWORelPath = Join-Path $repoRoot -ChildPath bin\Release\netstandard2.0\WOLAP.dll
+# Path to patch WOLAP.DependencyPatcher.csproj
+$paWOLAPPath = Join-Path $patchRoot "WOLAP.DependencyPatcher.csproj"
+# Path to patch WOLAP.DependencyPatcher.dll
+$paWORelPath = Join-Path $patchRoot -ChildPath bin\Release\net35\WOLAP.DependencyPatcher.dll
+# Path to manifest
+$maniPath = Join-Path $packagerRoot "manifest.json"
+# Path to pack README.md
+$readPath = Join-Path $packagerRoot "README.md"
+# Path to icon.png
+$iconPath = Join-Path $packagerRoot "icon.png"
+# Path to ref\Newtonsoft
+$newtPath = Join-Path $refRoot "Newtonsoft.Json.dll"
+
+
